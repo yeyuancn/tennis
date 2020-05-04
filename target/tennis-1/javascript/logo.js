c10b7c58ce8e43ae9e248a6e694e4844f8d76e41
yuan.ye <yuan.ye@servicenow.com>
2020-05-04 09:49:37 -0700

    remove target

--- target/tennis-1/javascript/logo.js
+++
@@ -1,7 +0,0 @@
-document.write('<div class="content header" ng-controller="sessionController">'
-        + '<!-- 940px width on desktop --> <div class="float logo">'
-        + '<span id="logo">{{firstName}}, Welcome to Tennis League: {{ leagueName }}</span>'
-        + '<span id="quote">"Love your favorite Tennis League"</span>'
-        + '<span id="since">{{ leagueName }} 2016</span>'
-        + '</div>'
-        + '</div> <p/>');
--- target/tennis-1/javascript/logo_static.js
+++
@@ -1,7 +0,0 @@
-document.write('<div class="content header">'
-        + '<!-- 940px width on desktop --> <div class="float logo">'
-        + '<a href="#" id="logo">Welcome to Just Tennis League</a>'
-        + '<span id="quote">"Managing your own Tennis League"</span>'
-        + '<span id="since">Just Tennis League 2016</span>'
-        + '</div>'
-        + '</div> <p/>');
